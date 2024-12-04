3f162ae2974a3bafcbbaa6e7461120af65c3428b
Narin <65040937+Narin4ik@users.noreply.github.com>
2024-12-04 15:16:25 +0200

    Update Config.cs (v1.3.0)

--- CustomHint/Config.cs
+++ CustomHint/Config.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.ComponentModel;
 using Exiled.API.Interfaces;
 using PlayerRoles;
@@ -19,18 +19,6 @@ namespace CustomHintPlugin
         [Description("The interval for changing spectator hints (in seconds).")]
         public float HintMessageTime { get; set; } = 5f;
 
-        [Description("Hint message for spectators.")]
-        public string HintMessageForSpectators { get; set; } = "{servername}\n{ip}:{port}\n\n{player_nickname}, spec, duration: {round_duration_hours}:{round_duration_minutes}:{round_duration_seconds}.\nRole: {player_role}\nTPS: {tps}/60\n\n{hints}";
-
-        [Description("Hint message for rounds lasting up to 59 seconds.")]
-        public string HintMessageUnderMinute { get; set; } = "{servername}\n{ip}:{port}\n\nQuick start! {player_nickname}, round time: {round_duration_seconds}s.\nRole: {player_role}\nTPS: {tps}/60";
-
-        [Description("Hint message for rounds lasting from 1 minute to 59 minutes and 59 seconds.")]
-        public string HintMessageUnderHour { get; set; } = "{servername}\n{ip}:{port}\n\nStill going, {player_nickname}! Time: {round_duration_minutes}:{round_duration_seconds}.\nRole: {player_role}\nTPS: {tps}/60";
-
-        [Description("Hint message for rounds lasting 1 hour or more.")]
-        public string HintMessageOverHour { get; set; } = "{servername}\n{ip}:{port}\n\nLong run, {player_nickname}! Duration: {round_duration_hours}:{round_duration_minutes}:{round_duration_seconds}.\nRole: {player_role}\nTPS: {tps}/60";
-
         [Description("Default role name for players without a role.")]
         public string DefaultRoleName { get; set; } = "Player";
 
@@ -47,32 +35,5 @@ namespace CustomHintPlugin
 
         [Description("Enable or disable HUD-related commands.")]
         public bool EnableHudCommands { get; set; } = true;
-
-        [Description("Message displayed when the HUD is successfully hidden.")]
-        public string HideHudSuccessMessage { get; set; } = "<color=green>You have successfully hidden the server HUD! To get the HUD back, use .showhud</color>";
-
-        [Description("Message displayed when the HUD is already hidden.")]
-        public string HideHudAlreadyHiddenMessage { get; set; } = "<color=red>You've already hidden the HUD server.</color>";
-
-        [Description("Message displayed when the HUD is successfully shown.")]
-        public string ShowHudSuccessMessage { get; set; } = "<color=green>You have successfully returned the server HUD! To hide again, use .hidehud</color>";
-
-        [Description("Message displayed when the HUD is already shown.")]
-        public string ShowHudAlreadyShownMessage { get; set; } = "<color=red>You already have the server HUD displayed.</color>";
-
-        [Description("Message displayed when DNT (Do Not Track) mode is enabled.")]
-        public string DntEnabledMessage { get; set; } = "<color=red>Disable DNT (Do Not Track) mode.</color>";
-
-        [Description("Message displayed when commands are disabled on the server.")]
-        public string CommandDisabledMessage { get; set; } = "<color=red>This command is disabled on the server.</color>";
-
-        [Description("Message displayed when the hint system is toggled on.")]
-        public string ToggleOnMessage { get; set; } = "<color=green>Hint system and commands are now enabled for this round.</color>";
-
-        [Description("Message displayed when the hint system is toggled off.")]
-        public string ToggleOffMessage { get; set; } = "<color=red>Hint system and commands are now disabled for this round.</color>";
-
-        [Description("Message displayed when commands are disabled for the round.")]
-        public string CommandsDisabledMessage { get; set; } = "<color=red>Commands .showhud and .hidehud are disabled for this round.</color>";
     }
 }
