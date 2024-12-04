722e13aa28970c9d9a000e2a9ddd348ba5a6d930
Narin <65040937+Narin4ik@users.noreply.github.com>
2024-12-04 14:04:46 +0200

    Update EventHandlers.cs (v1.2.2)

--- CustomHint/EventHandlers.cs
+++ CustomHint/EventHandlers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using MEC;
 using Exiled.API.Features;
 using Exiled.Events.EventArgs.Server;
@@ -12,6 +12,7 @@ namespace CustomHintPlugin
     public class EventHandlers
     {
         private CoroutineHandle _hintCoroutine;
+        private CoroutineHandle _spectatorHintCoroutine;
         private DateTime _roundStartTime;
         private bool _isRoundActive;
         private List<string> spectatorHints = new List<string>();
@@ -23,7 +24,9 @@ namespace CustomHintPlugin
                 Log.Debug("Waiting for players, disabling hints.");
 
             _isRoundActive = false;
+
             Timing.KillCoroutines(_hintCoroutine);
+
             Plugin.Instance.HiddenHudPlayers.Clear();
         }
 
@@ -36,7 +39,8 @@ namespace CustomHintPlugin
             _roundStartTime = DateTime.UtcNow;
 
             LoadSpectatorHints();
-            _hintCoroutine = Timing.RunCoroutine(UpdateHints());
+            StartSpectatorHints();
+            _hintCoroutine = Timing.RunCoroutine(ContinuousHintDisplay());
         }
 
         public void OnRoundEnded(RoundEndedEventArgs ev)
@@ -48,43 +52,78 @@ namespace CustomHintPlugin
             _roundStartTime = default;
 
             Timing.KillCoroutines(_hintCoroutine);
+            Timing.KillCoroutines(_spectatorHintCoroutine);
+
             randomizedHints.Clear();
-            Log.Debug("Cleared spectator hints queue.");
         }
 
-        private IEnumerator<float> UpdateHints()
+        public void OnPlayerVerified(Exiled.Events.EventArgs.Player.VerifiedEventArgs ev)
+        {
+            Player player = ev.Player;
+
+            if (player.DoNotTrack)
+            {
+                Log.Debug($"Player {player.Nickname} ({player.UserId}) has DNT enabled. HUD will be shown.");
+                return;
+            }
+
+            if (Plugin.Instance.HiddenHudPlayers.Contains(player.UserId))
+            {
+                Log.Debug($"Player {player.Nickname} ({player.UserId}) is in HiddenHudPlayers. HUD will be hidden.");
+
+                Plugin.Instance.IsHintSystemEnabled = false;
+            }
+            else
+            {
+                Log.Debug($"Player {player.Nickname} ({player.UserId}) is not in HiddenHudPlayers. HUD will be shown.");
+
+                Plugin.Instance.IsHintSystemEnabled = true;
+            }
+        }
+
+
+        private IEnumerator<float> ContinuousHintDisplay()
         {
             while (_isRoundActive)
             {
-                TimeSpan roundDuration = DateTime.UtcNow - _roundStartTime;
-
-                foreach (var player in Player.List)
+                if (Plugin.Instance.IsHintSystemEnabled)
                 {
-                    if (Plugin.Instance.IsHudHiddenForPlayer(player))
-                        continue;
+                    TimeSpan roundDuration = DateTime.UtcNow - _roundStartTime;
 
-                    if (player.Role.Type == RoleTypeId.Spectator ||
-                        (!Plugin.Instance.Config.ExcludedRoles.Contains(RoleTypeId.Overwatch) &&
-                         player.Role.Type == RoleTypeId.Overwatch))
+                    foreach (var player in Player.List)
                     {
-                        if (Plugin.Instance.Config.HintForSpectatorsIsEnabled)
+                        if (Plugin.Instance.IsHudHiddenForPlayer(player))
+                            continue;
+
+                        if (player.Role.Type == RoleTypeId.Spectator ||
+                            (!Plugin.Instance.Config.ExcludedRoles.Contains(RoleTypeId.Overwatch) &&
+                             player.Role.Type == RoleTypeId.Overwatch))
                         {
-                            UpdateSpectatorHint(player, roundDuration);
+                            if (Plugin.Instance.Config.HintForSpectatorsIsEnabled)
+                            {
+                                DisplayHintForSpectators(player, roundDuration);
+                            }
                         }
-                    }
-                    else if (!Plugin.Instance.Config.ExcludedRoles.Contains(player.Role.Type))
-                    {
-                        UpdatePlayerHint(player, roundDuration);
+                        else if (!Plugin.Instance.Config.ExcludedRoles.Contains(player.Role.Type))
+                        {
+                            DisplayHint(player, roundDuration);
+                        }
                     }
                 }
 
                 yield return Timing.WaitForSeconds(1f);
             }
         }
-        private void UpdatePlayerHint(Player player, TimeSpan roundDuration)
+
+        private void DisplayHint(Player player, TimeSpan roundDuration)
         {
             if (Plugin.Instance.IsHudHiddenForPlayer(player))
                 return;
+
+            if (randomizedHints.Count == 0)
+                return;
+
+            string currentHint = randomizedHints.Peek();
 
             string hintMessage = (roundDuration.TotalSeconds <= 59)
                 ? Plugin.Instance.Config.HintMessageUnderMinute
@@ -101,27 +140,20 @@ namespace CustomHintPlugin
                 .Replace("{tps}", Server.Tps.ToString("F1"))
                 .Replace("{servername}", Server.Name)
                 .Replace("{ip}", Server.IpAddress)
-                .Replace("{port}", Server.Port.ToString());
+                .Replace("{port}", Server.Port.ToString())
+                .Replace("{hints}", currentHint);
 
             hintMessage = Plugin.ReplaceColorsInString(hintMessage);
 
             player.ShowHint(hintMessage, 1f);
         }
 
-        private void UpdateSpectatorHint(Player player, TimeSpan roundDuration)
+        private void DisplayHintForSpectators(Player player, TimeSpan roundDuration)
         {
-            if (Plugin.Instance.IsHudHiddenForPlayer(player))
-                return;
-
-            if (randomizedHints.Count == 0 && spectatorHints.Count > 0)
-            {
-                randomizedHints = new Queue<string>(spectatorHints.OrderBy(_ => Guid.NewGuid()));
-            }
-
             if (randomizedHints.Count == 0)
                 return;
 
-            string currentHint = randomizedHints.Dequeue();
+            string currentHint = randomizedHints.Peek();
 
             string hintMessage = Plugin.Instance.Config.HintMessageForSpectators
                 .Replace("{round_duration_hours}", roundDuration.Hours.ToString("D2"))
@@ -145,6 +177,30 @@ namespace CustomHintPlugin
             return player.Group != null
                 ? $"<color={player.Group.BadgeColor ?? Plugin.Instance.Config.DefaultRoleColor}>{player.Group.BadgeText}</color>"
                 : $"<color={Plugin.Instance.Config.DefaultRoleColor}>{Plugin.Instance.Config.DefaultRoleName}</color>";
+        }
+
+        private void StartSpectatorHints()
+        {
+            _spectatorHintCoroutine = Timing.RunCoroutine(SpectatorHintUpdater());
+        }
+
+        private IEnumerator<float> SpectatorHintUpdater()
+        {
+            while (_isRoundActive)
+            {
+                if (randomizedHints.Count == 0 && spectatorHints.Count > 0)
+                {
+                    randomizedHints = new Queue<string>(spectatorHints.OrderBy(_ => Guid.NewGuid()));
+                    Log.Debug("Refilled spectator hints queue.");
+                }
+
+                if (randomizedHints.Count > 0)
+                {
+                    randomizedHints.Dequeue();
+                }
+
+                yield return Timing.WaitForSeconds(Plugin.Instance.Config.HintMessageTime);
+            }
         }
 
         public void LoadSpectatorHints()
@@ -173,33 +229,5 @@ namespace CustomHintPlugin
                 spectatorHints = new List<string>();
             }
         }
-
-        public static string ReplaceColorsInString(string input)
-        {
-            const string serverNamePlaceholder = "[SERVERNAME_PLACEHOLDER]";
-            input = input.Replace("{servername}", serverNamePlaceholder);
-
-            Dictionary<string, string> colorMapping = new Dictionary<string, string>
-            {
-                { "red", "#FF0000" },
-                { "green", "#00FF00" },
-                { "blue", "#0000FF" },
-                { "yellow", "#FFFF00" },
-                { "white", "#FFFFFF" },
-                { "black", "#000000" }
-            };
-
-            foreach (var pair in colorMapping)
-            {
-                string oldTag = $"<color={pair.Key}>";
-                string newTag = $"<color={pair.Value}>";
-                input = input.Replace(oldTag, newTag, StringComparison.OrdinalIgnoreCase);
-            }
-
-            input = input.Replace(serverNamePlaceholder, "{servername}");
-
-            return input;
-        }
-
     }
 }
